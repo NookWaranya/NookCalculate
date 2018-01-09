0bfa52664c82fdfa52586730878916d35fe9d201
Admin <nook09873@gmail.com>
2018-01-09 15:44:54 +0700

    Back Controller

--- app/src/main/java/waranya/krirk/ac/th/nookcalculate/fragment/CalculateFragment.java
+++ app/src/main/java/waranya/krirk/ac/th/nookcalculate/fragment/CalculateFragment.java
@@ -4,10 +4,12 @@ package waranya.krirk.ac.th.nookcalculate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import waranya.krirk.ac.th.nookcalculate.MainActivity;
 import waranya.krirk.ac.th.nookcalculate.R;
 
 /**
@@ -15,6 +17,34 @@ import waranya.krirk.ac.th.nookcalculate.R;
  */
 
 public class CalculateFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    } // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
 
     @Nullable
     @Override
